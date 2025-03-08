ae155fa8104752bdd91b55df9cd30284a518888c
veerappan711 <rrai2229@gmail.com>
2025-03-08 14:03:52 +0530

    Changes committed for first.py

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        
+        print("\n")
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter the number of terms: "))
+fibonacci(n)
